53e5ecc9a2ae442cfe55ef23191ed9bbe10e1ccf
Marcel Melo <marcel.msmelo@gmail.com>
2016-05-23 10:27:12 -0300

    remove config folder

--- config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = (mongoose) =>{
-  // const user = 'msmelo';
-  // const pass = 'm4rc3lsm';
-  // const url = 'mongodb://'+user+':'+pass+'@ds021771.mlab.com:21771/cardapio01';
-  const url = 'mongodb://127.0.0.1:27017/cardapio01';
-  mongoose.connect(url);
-}

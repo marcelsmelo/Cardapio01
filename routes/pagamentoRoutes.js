e16986e29071b564e7ac1effdad9aff539a3e094
Marcel Melo <marcel.msmelo@gmail.com>
2016-06-01 10:21:59 -0300

    Integracao notificacao pagseguro

--- routes/pagamentoRoutes.js
+++ routes/pagamentoRoutes.js
@@ -1,6 +1,7 @@
 module.exports = (app)=>{
   const controller = app.controllers.PagamentoController;
+  const enableCors = require('../lib/enableCORS.js');
 
   app.get('/pagamento/assinatura', controller.assinatura);
-  app.post('/pagamento/notificacao', controller.notificacao);
+  app.post('/pagamento/notificacao',enableCors,  controller.notificacao);
 }

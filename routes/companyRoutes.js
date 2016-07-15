0a6531d10bf60deabdd8ec21f4328f960cb0c02c
Marcel Melo <marcel.msmelo@gmail.com>
2016-07-15 13:29:57 -0300

    Upload file base64 to Amazon

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -8,6 +8,6 @@ module.exports = (app)=>{
   app.post('/company/changePassword', JWTPolicy, controller.changePassword);
   app.get('/company/generateTags', JWTPolicy, controller.generateTags);
   //app.post('/company/changeStatus', JWTPolicy, controller.changeStatus);
-  app.post('/company/uploadLogo', multerConfig.single('image'), JWTPolicy, controller.uploadLogo);
-  app.post('/company/uploadBanner', multerConfig.single('image'), JWTPolicy, controller.uploadBanner);
+  app.post('/company/uploadLogo', controller.uploadLogo);
+  app.post('/company/uploadBanner', controller.uploadBanner);
 }

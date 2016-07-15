6ba69ac4973ab1f0fe0dbbff891b566477030d72
msmelo <marcel.msmelo@gmail.com>
2016-07-15 13:34:11 -0400

    Bug corrigido, pegar imagem pelo req.body.image

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -171,7 +171,7 @@ module.exports = {
   //FIXME Retirar exemplo de upload de imagem do arquivo app.js e mover para companycontroller
 
   uploadLogo: (req, res, next)=>{
-    require('../lib/uploadImageS3.js')(req.companyID, req.image)
+    require('../lib/uploadImageS3.js')(req.companyID, req.body.image)
     .then((success)=>{
       //salvar url no BD
       success.msg = 'Logo da empresa enviado com sucesso!';
@@ -184,7 +184,7 @@ module.exports = {
   },
 
   uploadBanner: (req, res, next)=>{
-    require('../lib/uploadImageS3.js')(req.companyID, req.image)
+    require('../lib/uploadImageS3.js')(req.companyID, req.body.image)
     .then((success)=>{
       //salvar url no BD
       success.msg = 'Banner da empresa enviado com sucesso!';

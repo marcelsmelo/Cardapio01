c326372c288a0cab06b1e428a06b7a2b7c15b893
msmelo <marcel.msmelo@gmail.com>
2016-07-20 10:50:27 -0400

    Cancelar assinaturas pagseguro

--- controllers/PagseguroController.js
+++ controllers/PagseguroController.js
@@ -132,16 +132,17 @@ module.exports = {
  cancelar: (req, res, next)=>{
      const email = pagSeguroConfig.emailSandbox;
      const token = pagSeguroConfig.tokenSandbox;
-
+     console.log('here');
      Company.findOne({_id: '573b8cf7da7504af0ae33501'},{subscription:1, status:1})
      .then((company)=>{
+console.log('COMPANY', company);
          if(company.subscription.status == 'ACTIVE'){
-             let baseURL = 'https://ws.pagseguro.uol.com.br/v2/pre-approvals/cancel/';
+             let baseURL = 'https://ws.sandbox.pagseguro.uol.com.br/v2/pre-approvals/cancel/';
              let options = {
                uri: baseURL+company.subscription.code+'?email='+email+'&token='+token,
                method: 'GET'
              }
-
+	     console.log('OPTIONS', options);
              request(options, (errPS, responsePS, bodyPS) => {
                  let parse2json = xml2js.parseString;
                  parse2json(bodyPS, {'explicitArray': false}, (errParse, resultParse)=>{

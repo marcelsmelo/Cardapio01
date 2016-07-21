e738c3d97e604842d90f2f8f94cfbc6180fb306e
msmelo <marcel.msmelo@gmail.com>
2016-07-21 12:20:58 -0300

    Promises para gerar arquivo de tags e salvar no banco

--- controllers/TagsController.js
+++ controllers/TagsController.js
@@ -5,21 +5,11 @@ module.exports = {
     generateTags: (req, res, next) =>{
         require('../lib/generateTags.js')(req.companyID)
         .then((success)=>{
-            logger.debug('[Tags Controller]', 'Retorno S3', success);
-            Company.update({_id: companyID}, {$set: {tags: success.url}})
-            .then((company)=>{
-              logger.debug('[Tags Controller]', 'Salvar URL Tags', company);
-              success.msg = 'Arquivo de etiquetas criado com sucesso!';
-              res.status(200).json(success);
-            })
-            .catch((err)=>{
-              logger.error('[Tags Controller]', 'Erro ao salvar URL Tags', err);
-              res.status(500).json({success: false, msg: 'Erro ao salvar a URL do arquivo de etiquetas. Tente novamente!'});
-            })
+            logger.debug('[Tags Controller]', 'Retorno gerar arquivo Tags', success);
+            res.status(200).json(success);
         })
         .catch((err) => {
-            logger.error('[Tags Controller]', 'Erro ao salvar Tags S3', err);
-            err.msg = 'Erro ao criar arquivo de etiquetas. Tente novamente!';
+            logger.error('[Tags Controller]', 'Erro ao gerar arquivo de tags', err);
             res.status(500).json(err);
         });
     },

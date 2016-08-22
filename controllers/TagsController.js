bd81b73d6b503696344886cbce0c561299b97e8f
msmelo <marcel.msmelo@gmail.com>
2016-08-22 14:28:39 -0300

    save qrcode on amazon s3

--- controllers/TagsController.js
+++ controllers/TagsController.js
@@ -6,7 +6,8 @@ module.exports = {
         require('../lib/generateTags.js')(req.companyID)
             .then((success) => {
                 logger.debug('[Tags Controller]', 'Arquivo Tags gerado com sucesso', success);
-                res.status(200).json(success);
+				success.qrCode = `${amazonConfig.baseURL}/${params.bucket}/${req.companyID}_qrCode.png`
+				res.status(200).json(success);
             })
             .catch((err) => {
                 logger.error('[Tags Controller]', 'Erro ao gerar arquivo de tags', err);
@@ -23,11 +24,11 @@ module.exports = {
                 tags: 1
             })
             .then((company) => {
-                logger.debug('[Tags Controller]', 'URL de tags recuperada', company.tags);
+                logger.debug('[Tags Controller]', 'URL de tags recuperada');
                 res.status(200).json({
                     success: true,
                     url: company.tags,
-					qrCode: `${amazonConfig.baseURL}/${params.bucket}/${req.companyID}_qrCode.pdf`
+					qrCode: `${amazonConfig.baseURL}/${params.bucket}/${req.companyID}_qrCode.png`
                 });
             })
             .catch((err) => {
